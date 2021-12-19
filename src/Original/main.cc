9365b38e92c18896e587c97a1e8dafcbe9b00501
Vladimir Maksimovski <vlade.maksimovski@gmail.com>
2021-12-19 04:58:17 +0100

    Remove ground from original as well

--- src/Original/main.cc
+++ src/Original/main.cc
@@ -45,7 +45,7 @@ hittable_list random_scene() {
     hittable_list world;
 
     auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+    // world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
 
     for (int a = -11; a < 11; a++) {
         for (int b = -11; b < 11; b++) {
